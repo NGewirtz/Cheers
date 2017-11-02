578dd502ed70cd8815b6a14b334083c1971632cb
Neil Gewirtz <ngewirtz86@gmail.com>
2017-11-02 09:50:36 -0400

    update checkin model for avg brew

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -6,10 +6,10 @@ class Checkin < ApplicationRecord
   belongs_to :beer
   has_many :comments
   has_many :cheers
-  after_create :update_beer_avg
+  after_create :update_beer_avg, :update_brew_avg
 
-  before_update :remove_beer_avg
-  after_update :update_beer_avg
+  before_update :remove_beer_avg, :remove_brew_avg
+  after_update :update_beer_avg, :update_brew_avg
 
   has_one :brewery,
     through: :beer,
@@ -26,6 +26,16 @@ class Checkin < ApplicationRecord
     beer.update!(avg_rating: avg)
   end
 
+  def remove_brew_avg
+    brewery = Brewery.find(self.beer.brewery.id)
+    if brewery.ratings == 1
+      avg = nil
+    else
+      avg = (((brewery.avg_rating.to_f * (brewery.ratings.to_f))) - user.checkins.find(self.id).rating.to_f ) / (brewery.ratings.to_f - 1)
+    end
+    brewery.update!(avg_rating: avg)
+  end
+
   def update_beer_avg
     beer = Beer.find(self.beer_id)
     if !beer.avg_rating
@@ -34,5 +44,15 @@ class Checkin < ApplicationRecord
       avg = (((beer.avg_rating.to_f * (beer.ratings.to_f - 1 ))) + self.rating.to_f ) / (beer.ratings.to_f)
     end
     beer.update!(avg_rating: avg)
+  end
+
+  def update_brew_avg
+    brewery = Brewery.find(self.beer.brewery.id)
+    if !brewery.avg_rating
+      avg = self.rating
+    else
+      avg = (((brewery.avg_rating.to_f * (brewery.ratings.to_f - 1 ))) + self.rating.to_f ) / (brewery.ratings.to_f)
+    end
+    brewery.update!(avg_rating: avg)
   end
 end
