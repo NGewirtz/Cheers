e866a2cf5c212924513850b1b2e9d03fd7e7d28d
Neil Gewirtz <ngewirtz86@gmail.com>
2017-12-06 14:43:01 -0500

    add footer links

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-
+      <a href="https://github.com/NGewirtz">
+        <i class="fa fa-github fa-fw fa-3x"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/neilgewirtz/">
+        <i class="fa fa-linkedin-square fa-fw fa-3x"></i>
+      </a>
     </footer>
   );
 };
