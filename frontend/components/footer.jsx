300211cdbd6562badcb854a5d75e35efb8f26adf
Neil Gewirtz <ngewirtz86@gmail.com>
2017-12-14 18:39:42 -0500

    small fixes

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -3,13 +3,15 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <a href="https://github.com/NGewirtz">
-        <i class="fa fa-github fa-fw fa-3x"></i>
-      </a>
-      <a href="https://www.linkedin.com/in/neilgewirtz/">
-        <i class="fa fa-linkedin-square fa-fw fa-3x"></i>
-      </a>
-      <a href="http://www.neilgewirtz.com">By: Neil Gewirtz</a>
+      <nav>
+        <a href="https://github.com/NGewirtz">
+          <i class="fa fa-github fa-fw fa-3x"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/neilgewirtz/">
+          <i class="fa fa-linkedin-square fa-fw fa-3x"></i>
+        </a>
+        <a href="http://www.neilgewirtz.com">By: Neil Gewirtz</a>
+      </nav>
     </footer>
   );
 };
