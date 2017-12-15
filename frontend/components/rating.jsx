d5ac0110fc84e1d92f7bf71531b7a9a4eb047cb2
Neil Gewirtz <ngewirtz86@gmail.com>
2017-12-15 16:33:31 -0500

    refactor rating component

--- frontend/components/rating.jsx
+++ frontend/components/rating.jsx
@@ -5,8 +5,12 @@ const Rating = ({rating}) => {
   const width = rating * 20 + "%";
   return (
     <div className="star-ratings-css">
-      <div className="star-ratings-css-top" style={{width}}><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
-      <div className="star-ratings-css-bottom"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
+      <div className="star-ratings-css-top"
+        style={{width}}><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
+      </div>
+      <div className="star-ratings-css-bottom">
+        <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
+      </div>
     </div>
   );
 };
