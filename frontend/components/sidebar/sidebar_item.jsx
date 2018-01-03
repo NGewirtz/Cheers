9701df77359b2c4b26cdfc9efeb585d4e111f550
Neil Gewirtz <ngewirtz86@gmail.com>
2018-01-02 19:18:55 -0500

    code cleanup

--- frontend/components/sidebar/sidebar_item.jsx
+++ frontend/components/sidebar/sidebar_item.jsx
@@ -6,7 +6,9 @@ const SidebarItem = ({beer}) => (
     <img src={beer.image}/>
     <div>
       <Link to={`/beers/${beer.id}`}><h3>{beer.name}</h3></Link>
-      <Link to={`/breweries/${beer.brewery_id}`}><h4>{beer.breweryName}</h4></Link>
+      <Link to={`/breweries/${beer.brewery_id}`}>
+        <h4>{beer.breweryName}</h4>
+      </Link>
     </div>
   </li>
 );
